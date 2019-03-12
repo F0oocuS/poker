f2c5bf1108557acc5e10498ffce3e1b658239fc4
F0oocus <ihoryaremko@gmail.com>
2019-03-12 13:33:26 +0200

    add material to project

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './global/header/header.component';
+import { FooterComponent } from './global/footer/footer.component';
+import { PageNotFoundComponent } from './global/page-not-found/page-not-found.component';
+import { HomeComponent } from './global/home/home.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		PageNotFoundComponent,
+		HomeComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MaterialModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
+
 export class AppModule {}
